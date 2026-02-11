2442bb5fd2d2ba33ebb3d6306d1d69e224af8bde
Manoj Muglikar <manojmuglikar44@gmail.com>
2026-02-11 22:48:38 +0530

    Update Exercise 13.py

--- Exercise 13.py
+++ Exercise 13.py
@@ -1,4 +1,11 @@
-numbers = [-4, "-3", "-2", "-1", 0, 2, 4, 6]
+num = [-4, "-3", "-2", "-1", 0, 2, 4, 6]
 negative_or_zero = [int(num) for num in numbers if isinstance(num, (int, str)) and int(num) <= 0]
 print(negative_or_zero)
 ---> [-4, -3, -2, -1, 0]
+
+lst_of_lst = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened = [item for sublist in lst_of_lst for item in sublist]
+print(flattened)
+---> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
